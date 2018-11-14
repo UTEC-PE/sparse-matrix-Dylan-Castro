31c4de707c9b8cb45640d25fbee783670b5da0b6
unknown <dylan.castro@utec.edu.pe>
2018-11-13 21:08:31 -0500

    update

--- main.cpp
+++ main.cpp
@@ -7,29 +7,22 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     Matrix<int> a(5, 5);
-    Matrix<int> b(5, 5);
-    //b.set(3,3,5);
-    //b.set(4,3,20);
+    Matrix<int> b(8, 8);
+    b.set(2,2,5);
+    b.set(1,2,20);
     a.set(0,0,5);
-    //a.set(0,3,0);
     a.set(1,0,10);
     a.set(2,0,7);
     a.set(3,0,11);
     a.set(4,0,15);
     a.set(0,4,8);
-    a.set(0,0,0);
     //a=b;
-    //a.operator-(b);
-    //a.operator=(b);
-    //a.operator*(7);
-    //  a.transposed();
-    //a.print();
-    for(int x=0;x<5;x++){
-        for(int y=0;y<5;y++){
-            cout << a(x,y) << " ";
-        }
-        cout << endl;
-    }
+    //a=a*7;
+    //a=a*a;
+    //a=a+b;
+    //a=a-b;
+    //a=a.transposed();
+    a.print();
     system("PAUSE");
     return EXIT_SUCCESS;
 }
--- matrix.h
+++ matrix.h
@@ -7,17 +7,22 @@ class Matrix {
     private:
         Node<T>* hRows;
         Node<T>* hColumns;
+    public:
         int columns;
         int rows;
-    public:
-        Matrix();
+        Matrix(){
+            hRows=nullptr;
+            hColumns=nullptr;
+            columns=0;
+            rows=0;
+        };
         Matrix(int sizeX, int sizeY){
             hRows=new Node<T>;
             hColumns= new Node<T>;
             Node<T>** tempx= &hRows;
             Node<T>** tempy= &hColumns;
-            columns=sizeX;
-            rows=sizeY;
+            columns=sizeY;
+            rows=sizeX;
             for(int x=1;x<rows;x++){
                 tempx=&(*tempx)->down;
                 *tempx=new Node<T>;
@@ -60,6 +65,9 @@ class Matrix {
             while((*fila)->x < x);
 
             if(data==0){
+                if(this->operator()(x,y)==0){
+                    return;
+                }
                 Node<T>* temp= (*nodo_x_anterior)->down;
                 (*nodo_x_anterior)->down= (*nodo_x_anterior)->down->down;
                 (*nodo_y_anterior)->next= (*nodo_y_anterior)->next->next;
@@ -75,15 +83,21 @@ class Matrix {
         };
 
         void print(){
-            cout << hColumns->next->next->next->next->down->get();
-            cout << hColumns->down->get();
-            cout << hColumns->down->down->get();
-            cout << hColumns->down->down->down->get();
-            cout << hColumns->down->down->down->down->get();
-            cout << hColumns->down->down->down->down->down->get();
+            for(int x=0;x<rows;x++){
+                for(int y=0;y<columns;y++){
+                    cout << this->operator()(x,y) << " ";
+                }
+                cout << endl;
+            }
+
         }
         T operator()(int x, int y){
-             Node<T>** fila= &hRows;
+            if(x > rows-1 || y > columns-1){
+                cout << "Las dimensiones ingresadas no coinciden con la matrix :(" << endl;
+                system("PAUSE");
+                exit(true);
+            }
+            Node<T>** fila= &hRows;
             Node<T>** columna= &hColumns;
             for(int i=0;i<x;i++){
                 fila=&(*fila)->down;
@@ -104,21 +118,42 @@ class Matrix {
                 return false;
             }
         };
-        Matrix<T> operator*(Matrix<T> other);
+        Matrix<T> operator*(Matrix<T> other){
+            Matrix<int> c(rows,other.columns);
+            int suma=0;
+            for(int x=0;x<rows;x++){
+                for(int y=0;y<other.columns;y++){
+                    suma=0;
+                    for(int z=0;z<columns;z++){
+                        suma+=this->operator()(x,z)*other.operator()(z,y);
+                    }
+                    c.set(x,y,suma);
+
+                }
+            }
+            return c;
+        };
         Matrix<T> operator*(T scalar){
-            for(int x=0;x<5;x++){
-                for(int y=0;y<5;y++){
+            Matrix<int> c(rows,columns);
+            for(int x=0;x<rows;x++){
+                for(int y=0;y<columns;y++){
                     if(this->operator()(x,y)!=0){
-                        set(x,y,this->operator()(x,y)*scalar);
+                        c.set(x,y,this->operator()(x,y)*scalar);
                     }
                 }
             }
+            return c;
         };
         Matrix<T> operator+(Matrix<T> other){
-            Matrix<int> c(5,5);
+            if(this->rows != other.rows || this->columns != other.columns){
+                cout << "Las dimensiones de las matrices son diferentes :(" << endl;
+                system("PAUSE");
+                exit(true);
+            }
+            Matrix<int> c(rows,columns);
             int suma=0;
-            for(int x=0;x<5;x++){
-                for(int y=0;y<5;y++){
+            for(int x=0;x<rows;x++){
+                for(int y=0;y<columns;y++){
                     if(this->operator()(x,y)!=0 || other.operator()(x,y)!=0){
                         c.set(x,y,this->operator()(x,y)+ other.operator()(x,y));
                     }
@@ -127,10 +162,15 @@ class Matrix {
             return c;
         };
         Matrix<T> operator-(Matrix<T> other){
-            Matrix<int> c(5,5);
+            if(this->rows != other.rows || this->columns != other.columns){
+                cout << "Las dimensiones de las matrices son diferentes :(" << endl;
+                system("PAUSE");
+                exit(true);
+            }
+            Matrix<int> c(rows,columns);
             int resta=0;
-            for(int x=0;x<5;x++){
-                for(int y=0;y<5;y++){
+            for(int x=0;x<rows;x++){
+                for(int y=0;y<columns;y++){
                     if(this->operator()(x,y)!=0 || other.operator()(x,y)!=0){
                         c.set(x,y,this->operator()(x,y) - other.operator()(x,y));
                     }
@@ -139,24 +179,73 @@ class Matrix {
             return c;
         };
         Matrix<T> transposed(){
-            Matrix<T> temp(rows,columns);
+            Matrix<T> c(rows,columns);
             for(int x=0;x<rows;x++){
                 for(int y=0;y<columns;y++){
                     if(this->operator()(x,y)!=0){
-                        temp.set(y,x,this->operator()(x,y));
+                        c.set(y,x,this->operator()(x,y));
                     }
                 }
             }
-            return temp;
+            return c;
+        };
+        void Resize_matrix(int x,int y){
+            Node<T>* pointer= hColumns;
+            Node<T>* temp;
+            for(int j=0;j<y;j++){
+                pointer=pointer->next;
+                if(pointer->next==nullptr){
+                    break;
+                }
+            }
+            if(columns>y){
+                for(int j=y;j<columns;j++){
+                    pointer=pointer->next;
+                    temp=pointer;
+                    delete temp;
+                }
+            }
+            else{
+                for(int j=columns;j<y;j++){
+                    pointer->next = new Node<T>;
+                    pointer=pointer->next;
+                }
+            }
+            pointer= hRows;
+            temp=nullptr;
+            for(int i=0;i<x;i++){
+                pointer=pointer->down;
+                if(pointer->down==nullptr){
+                    break;
+                }
+            }
+            if(rows>x){
+                for(int i=x;i<rows;i++){
+                    pointer=pointer->down;
+                    temp=pointer;
+                    delete temp;
+                }
+            }
+            else{
+                for(int i=rows;i<x;i++){
+                    pointer->down = new Node<T>;
+                    pointer = pointer->down;
+                }
+            }
         };
         Matrix<T> operator=(Matrix<T> other){
-            for(int x=0;x<5;x++){
-                for(int y=0;y<5;y++){
+            for(int x=0;x<rows;x++){
+                for(int y=0;y<columns;y++){
                     set(x,y,0);
                 }
             }
-            for(int x=0;x<5;x++){
-                for(int y=0;y<5;y++){
+            if(this->rows != other.rows || this->columns != other.columns){
+                Resize_matrix(other.rows,other.columns);
+                rows=other.rows;
+                columns=other.columns;
+            }
+            for(int x=0;x<rows;x++){
+                for(int y=0;y<columns;y++){
                     if(other.operator()(x,y)!=0){
                         set(x,y,other.operator()(x,y));
                     }
@@ -164,6 +253,8 @@ class Matrix {
             }
             return *this;
         };
-        //~Matrix();
+        ~Matrix(){
+            //Resize_matrix(0,0);
+        };
 };
 #endif
